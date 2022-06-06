a0fdc85b9cb6b515d27fe82c9adaa878dedc2692
Zsombor Szende <szende.zsombor@alerant.hu>
2022-06-06 18:43:37 +0200

    Build added to mfp

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfiguration = require('./webpack.common');
 const packageJsonDependencies = require('../package.json').dependencies;
@@ -20,9 +19,6 @@ const devConfig = {
             },
             // shared: ['react', 'react-dom'],
             shared: packageJsonDependencies,
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
         }),
     ],
 }
